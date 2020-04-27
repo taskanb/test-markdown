04c6a279d8bff8d851f6e17e9c88721564cad399
Burcu Taskan <ultrascan35@hotmail.com>
2020-04-27 16:20:49 -0500

    Create md_to_docx.sh

---
+++ md_to_docx.sh
@@ -0,0 +1,10 @@
+FILES=*.md
+for f in $FILES
+do
+  # extension="${f##*.}"
+  filename="${f%.*}"
+  echo "Converting $f to $filename.rst"
+  `pandoc $f markdown -t docx -o $filename.docx $filename.md`
+  # uncomment this line to delete the source file.
+  # rm $f
+done
