9d75813972f29f2e1a652dbcaefbb932c99291cc
Burcu Taskan <ultrascan35@hotmail.com>
2020-05-13 15:02:28 -0500

    Create md_to_docx.sh

---
+++ md_to_docx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILES=*.md
+for f in $FILES
+do
+  # extension="${f##*.}"
+  filename="${f%.*}"
+  echo "Converting $f to $filename.docx"
+  `pandoc $f markdown -t docx -o $filename.docx $filename.md`
+  # uncomment this line to delete the source file.
+  # rm $f
+done 
