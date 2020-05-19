7ef03855ab0303ad934166e47909445be7e7ea13
Burcu Taskan <ultrascan35@hotmail.com>
2020-05-19 14:00:40 -0500

    Update md_to_docx.sh

--- md_to_docx.sh
+++ md_to_docx.sh
@@ -4,6 +4,7 @@ for f in $FILES
 do
   # extension="${f##*.}"
   filename="${f%.*}"
+  git add --chmod=+ $filename
   echo "Converting $f to $filename.docx"
   `pandoc $f markdown -t docx -o $filename.docx $filename.md`
   # uncomment this line to delete the source file.
